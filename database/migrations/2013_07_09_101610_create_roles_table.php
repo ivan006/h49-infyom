6bff155edffe0e26fc1e51a52347740af03d5ec1
ivan006 <ivan.copeland2015@gmail.com>
2020-07-09 15:46:02 +0200

    1

--- database/migrations/2013_07_09_101610_create_roles_table.php
+++ database/migrations/2013_07_09_101610_create_roles_table.php
@@ -17,7 +17,7 @@ class CreateRolesTable extends Migration
             $table->increments('id');
             $table->timestamps();
 
-            $table->string('name');
+            $table->string('name'); // Author, Admin. Normal User
             $table->softDeletes();
         });
     }
